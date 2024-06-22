6453f4131832b0f593e9e78a8903610c7bc8fcd5
Mondeep1999 <90750345+Mondeep1999@users.noreply.github.com>
2024-06-22 17:11:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Mondeep here!\n'
+  const msg = 'Hi Mondeep here!\n'
   res.end(msg);
 });
 
