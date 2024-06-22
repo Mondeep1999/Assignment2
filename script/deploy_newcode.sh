98e986dd5004a9dda5a845774759751d34b1594a
Ubuntu <ubuntu@ip-172-31-30-156.ec2.internal>
2024-06-22 11:40:12 +0000

    3rd push

--- script/deploy_newcode.sh
+++ script/deploy_newcode.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 
 cd ~/Assignment2
 git pull origin main
 docker build -t hello-node-image .
 docker kill my-node-app
+docker system prune
 docker run --name my-node-app -d -p 3000:3000 hello-node-image
